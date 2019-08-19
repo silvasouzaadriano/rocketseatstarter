a9cbcf3ea98c3841ce80be8b32c4b9813cc285d1
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-19 15:51:29 -0300

    Starter - Javascript - ES - Variaveis

--- curso-es6/bundle.js
+++ curso-es6/bundle.js
@@ -1,30 +1,22 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+// var = Constantes não podem ter seu valor reatribuido, mas podem ser mutados
+//const a = 1; 
+//a = 3; // Isso daria erro pois o valor está sendo reatribuido
+//Mutação
+var usuario = {
+  nome: 'Adriano'
+};
+usuario.nome = 'Diego';
+console.log(usuario); // let = Variáveis let só podem ser usadas dentro do escopo, exemplo dentro de funções.
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function teste(x) {
+  var y = 2;
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-
-    this.todos = [];
+  if (x > 5) {
+    var _y = 4;
+    console.log(x, _y);
   }
+}
 
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      //static são métodos que não alteram o conteúdo mas realizam ações com eles
-      // retornando resultados
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2));
+teste(10);
--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,12 +1,24 @@
-class Matematica {
-  constructor() {
-    this.todos = [];
+// var = Constantes não podem ter seu valor reatribuido, mas podem ser mutados
+//const a = 1; 
+//a = 3; // Isso daria erro pois o valor está sendo reatribuido
+
+//Mutação
+const usuario = { nome: 'Adriano'};
+usuario.nome = 'Diego';
+console.log(usuario);
+
+
+// let = Variáveis let só podem ser usadas dentro do escopo, exemplo dentro de funções.
+function teste(x) {
+  let y = 2;
+
+  if (x > 5) {
+    let y = 4
+    console.log(x, y);
   }
 
-  static soma(a, b) { //static são métodos que não alteram o conteúdo mas realizam ações com eles
-                      // retornando resultados
-    return a + b;
-  }
 }
 
-console.log(Matematica.soma(1,2));
+teste(10);
+
+
