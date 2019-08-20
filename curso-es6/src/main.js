96612188b73c7500b6408347a500cfceef1e9f2d
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-20 17:31:06 -0300

    Starter - Javascript - ES - App - Loading e Error

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -15,6 +15,19 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode(' Carregando...'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository() {
     event.preventDefault();
 
@@ -22,23 +35,31 @@ class App {
 
     if (repoInput.length === 0)  // Quando não tem informação a função é interrompida
       return;
+
+    this.setLoading();  
     
-    const response = await api.get(`/repos/${repoInput}`);
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const {name, description, html_url, owner: { avatar_url} } = response.data;
-  
-    console.log(response);
+      const {name, description, html_url, owner: { avatar_url} } = response.data;
+    
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
+      this.inputEl.value = '';
 
-    this.inputEl.value = '';
+      this.render()
+    } catch (err) {
+      alert(`O repositório ${this.inputEl.value} não existe!`);
+      this.inputEl.value = '';
+    }
 
-    this.render()
+    this.setLoading(false);
+
   }
 
   render() {
